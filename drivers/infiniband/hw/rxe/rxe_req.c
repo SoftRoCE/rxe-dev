f3c2d9baa568019b9741e75cf33df2e7d4a08995
Kamal Heib <kamalh@mellanox.com>
2015-02-16 13:19:47 +0200

    rxe: Fix the check of max unAcked PSNs
    Fix the check of max unAcked PSNs instead
    of using psn_compare function, turn on wait_psn
    when req.psn > comp.psn + MAX_UNACKED.
    
    Fixes: #23
    Issue: None
    
    Change-Id: I0352b7859a0a15acf5ac278f31f6c68617e5a5e4
    Signed-off-by: Kamal Heib <kamalh@mellanox.com>

--- drivers/infiniband/hw/rxe/rxe_req.c
+++ drivers/infiniband/hw/rxe/rxe_req.c
@@ -629,8 +629,7 @@ int rxe_requester(void *arg)
 	 * half of the PSN range (2^24).
 	 */
 	if (qp_type(qp) == IB_QPT_RC)  {
-		if (psn_compare(qp->req.psn, qp->comp.psn +
-				RXE_MAX_UNACKED_PSNS) > 0) {
+		if (qp->req.psn > (qp->comp.psn + RXE_MAX_UNACKED_PSNS)) {
 			qp->req.wait_psn = 1;
 			goto exit;
 		}
