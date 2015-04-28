yonicswe/linux

IB/core: Report gid_type and gid_ndev through sysfs
Since we've added GID attributes to the RoCE GID table,
the users need a convenient way to query them.
Adding the GID type and relate net device to IB's sysfs.

The new attributes are available in:
/sys/class/infiniband/<device>/ports/<port>/gid_attrs/ndevs/<index>
/sys/class/infiniband/<device>/ports/<port>/gid_attrs/types/<index>

The <index> corresponds to the index of the respective GID in:
/sys/class/infiniband/<device>/ports/<port>/gids/<index>

Change-Id: I18dc41333ae1778121e870b64a8d737f1ceac77b
Signed-off-by: Matan Barak <matanb@mellanox.com>

drivers/infiniband/core/roce_gid_cache.c
@@ -48,6 +48,11 @@ enum gid_attr_find_mask {
 	GID_ATTR_FIND_MASK_NETDEV	= 1UL << 1,
 };
 
+static const char * const gid_type_str[] = {
+	[IB_GID_TYPE_IB]	= "IB/RoCE V1\n",
+	[IB_GID_TYPE_ROCE_V2]	= "RoCE V2\n",
+};
+
 static inline int start_port(struct ib_device *ib_dev)
 {
 	return (ib_dev->node_type == RDMA_NODE_IB_SWITCH) ? 0 : 1;
@@ -57,6 +62,14 @@ struct dev_put_rcu {
 	struct rcu_head		rcu;
 	struct net_device	*ndev;
 };
+
+const char *roce_gid_cache_type_str(enum ib_gid_type gid_type)
+{
+	if (gid_type < ARRAY_SIZE(gid_type_str) && gid_type_str[gid_type])
+		return gid_type_str[gid_type];
+
+	return "Invalid GID type";
+}
 
 static void put_ndev(struct rcu_head *rcu)
 {

SoftRoCE/rxe-dev

Change-Id: I9a29bac0ca0ab0e38e2e9c67c424886d5cbbf376

drivers/infiniband/core/roce_gid_cache.c
@@ -50,6 +50,11 @@ enum gid_attr_find_mask {
 	GID_ATTR_FIND_MASK_DEFAULT	= 1UL << 3,
 };
 
+static const char * const gid_type_str[] = {
+	[IB_GID_TYPE_IB]	= "IB/IBOE V1\n",
+	[IB_GID_TYPE_IBOE_V2]	= "IBOE V2\n",
+};
+
 static inline int start_port(struct ib_device *ib_dev)
 {
 	return (ib_dev->node_type == RDMA_NODE_IB_SWITCH) ? 0 : 1;
@@ -59,6 +64,14 @@ struct dev_put_rcu {
 	struct rcu_head		rcu;
 	struct net_device	*ndev;
 };
+
+const char *roce_gid_cache_type_str(enum ib_gid_type gid_type)
+{
+	if (gid_type < ARRAY_SIZE(gid_type_str) && gid_type_str[gid_type])
+		return gid_type_str[gid_type];
+
+	return "Invalid GID type";
+}
 
 static void put_ndev(struct rcu_head *rcu)
 {